00699a7a9572f8a59400a0eef872865feaf564f5
0xC0000054 <0xC0000054@users.noreply.github.com>
2014-06-12 06:15:50 +0000

    Allow the creator block to be written to a PSP file
    Removed unused usings and marked the class as sealed.
    Marked the class as serializable.
    Renamed the desc field.
    Changed the file time stamp to use local time.

--- PSPSections/CreatorBlock.cs
+++ PSPSections/CreatorBlock.cs
@@ -1,34 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.IO;
+using System.Text;
 
 namespace PaintShopProFiletype.PSPSections
 {
-	class CreatorBlock
+	[Serializable]
+	internal sealed class CreatorBlock
 	{
 		private string title;
 		private uint createDate;
+		[NonSerialized]
 		private uint modDate;
 		private string artist;
 		private string copyRight;
-		private string desc;
+		private string description;
+		[NonSerialized]
 		private PSPCreatorAppID appID;
+		[NonSerialized]
 		private uint appVersion;
 
+		[NonSerialized]
 		private uint blockLength;
 
-		private static readonly DateTime unixEpochUtc = new DateTime(1970, 1, 1).ToUniversalTime();
+		private static readonly DateTime unixEpochLocal = new DateTime(1970, 1, 1).ToLocalTime();
 		/// <summary>
-		/// Gets the current Unix Timestamp for the createDate and modDate time stamps 
+		/// Gets the current date and time in Unix format (the number of seconds from 1/1/1970)
 		/// </summary>
-		/// <returns>The datestamp in Unix format</returns> 
+		/// <returns>The current date and time in Unix format</returns> 
 		private static uint GetCurrentUnixTimestamp()
 		{
-			TimeSpan t = (DateTime.UtcNow - unixEpochUtc);
+			TimeSpan t = (DateTime.Now - unixEpochLocal);
 			
 			return (uint)t.TotalSeconds;
+		}
+
+		private static uint GetAppVersionPacked()
+		{
+			Version version = typeof(CreatorBlock).Assembly.GetName().Version;
+
+			return (uint)((version.Major << 24) | (version.Minor << 16) | (version.Build << 8) | version.Revision);
 		}
 
 #if DEBUG
@@ -36,7 +46,7 @@ namespace PaintShopProFiletype.PSPSections
 		{
 			get
 			{
-				return unixEpochUtc.AddSeconds(createDate);
+				return unixEpochLocal.AddSeconds(createDate);
 			}
 		}
 
@@ -44,21 +54,19 @@ namespace PaintShopProFiletype.PSPSections
 		{ 
 			get
 			{
-				return unixEpochUtc.AddSeconds(modDate);
+				return unixEpochLocal.AddSeconds(modDate);
 			}
 		}
-		
 #endif
 
 		public CreatorBlock()
 		{
-			this.blockLength = 28; // 5 UInt32 values
-
 			this.title = string.Empty;
-			this.createDate = this.modDate = GetCurrentUnixTimestamp();
+			this.createDate = GetCurrentUnixTimestamp();
+			this.modDate = 0U;
 			this.artist = string.Empty;
 			this.copyRight = string.Empty;
-			this.desc = string.Empty;
+			this.description = string.Empty;
 			this.appID = PSPCreatorAppID.Unknown;
 			this.appVersion = 1;
 		}
@@ -75,13 +83,13 @@ namespace PaintShopProFiletype.PSPSections
 
 			while (br.BaseStream.Position < endOffset && br.ReadUInt32() == PSPConstants.fieldIdentifier)
 			{
-				ushort fieldID = br.ReadUInt16();
+				PSPCreatorFieldID field = (PSPCreatorFieldID)br.ReadUInt16();
 				uint fieldLength = br.ReadUInt32();
-				PSPCreatorFieldID field = (PSPCreatorFieldID)fieldID;
+
 				switch (field)
 				{
 					case PSPCreatorFieldID.Title:
-						this.title = Encoding.ASCII.GetString(br.ReadBytes((int)fieldLength));
+						this.title = Encoding.ASCII.GetString(br.ReadBytes((int)fieldLength)).Trim();
 						break;
 					case PSPCreatorFieldID.CreateDate:
 						this.createDate = br.ReadUInt32();
@@ -90,13 +98,13 @@ namespace PaintShopProFiletype.PSPSections
 						this.modDate = br.ReadUInt32();
 						break;
 					case PSPCreatorFieldID.Artist:
-						this.artist = Encoding.ASCII.GetString(br.ReadBytes((int)fieldLength));
+						this.artist = Encoding.ASCII.GetString(br.ReadBytes((int)fieldLength)).Trim();
 						break;
 					case PSPCreatorFieldID.Copyright:
-						this.copyRight = Encoding.ASCII.GetString(br.ReadBytes((int)fieldLength));
+						this.copyRight = Encoding.ASCII.GetString(br.ReadBytes((int)fieldLength)).Trim();
 						break;
 					case PSPCreatorFieldID.Description:
-						this.desc = Encoding.ASCII.GetString(br.ReadBytes((int)fieldLength));
+						this.description = Encoding.ASCII.GetString(br.ReadBytes((int)fieldLength)).Trim();
 						break;
 					case PSPCreatorFieldID.ApplicationID:
 						this.appID = (PSPCreatorAppID)br.ReadUInt32();
@@ -108,6 +116,60 @@ namespace PaintShopProFiletype.PSPSections
 						break;
 				} 
 			}
+		}
+
+		private static void WriteASCIIField(BinaryWriter writer, PSPCreatorFieldID field, string value)
+		{
+			writer.Write(PSPConstants.fieldIdentifier);
+			writer.Write((ushort)field);
+
+			byte[] bytes = Encoding.ASCII.GetBytes(value);
+			
+			writer.Write((uint)bytes.Length);
+			writer.Write(bytes);
+		}
+
+		public void Save(BinaryWriter writer)
+		{
+			writer.Write(PSPConstants.blockIdentifier);
+			writer.Write((ushort)PSPBlockID.Creator);
+
+			using (new PSPUtil.BlockLengthWriter(writer))
+			{
+				if (!string.IsNullOrEmpty(title))
+				{
+					WriteASCIIField(writer, PSPCreatorFieldID.Title, this.title);
+				}
+
+				if (createDate != 0U)
+				{
+					writer.Write(PSPConstants.fieldIdentifier);
+					writer.Write((ushort)PSPCreatorFieldID.CreateDate);
+					writer.Write(sizeof(uint));
+					writer.Write(this.createDate);
+				}
+				
+				writer.Write(PSPConstants.fieldIdentifier);
+				writer.Write((ushort)PSPCreatorFieldID.ModifiedDate);
+				writer.Write(sizeof(uint));
+				writer.Write(GetCurrentUnixTimestamp());
+
+				if (!string.IsNullOrEmpty(artist))
+				{
+					WriteASCIIField(writer, PSPCreatorFieldID.Artist, this.artist);
+				}
+
+				if (!string.IsNullOrEmpty(copyRight))
+				{
+					WriteASCIIField(writer, PSPCreatorFieldID.Copyright, this.copyRight);
+				}
+
+				if (!string.IsNullOrEmpty(description))
+				{
+					WriteASCIIField(writer, PSPCreatorFieldID.Description, this.description);
+				}
+			}
+			
 		}
 	}
 }
