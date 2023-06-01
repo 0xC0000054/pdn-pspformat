51c9516df2731762379fd38cd86f9dee38ed95d0
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2023-05-31 23:37:19 -0600

    Bump copyright year

--- PSPSections/GeneralImageAttributes.cs
+++ PSPSections/GeneralImageAttributes.cs
@@ -2,7 +2,7 @@
 //
 // This file is part of pdn-pspformat, a FileType plugin for Paint.NET
 //
-// Copyright (c) 2011-2015, 2019 Nicholas Hayes
+// Copyright (c) 2011-2015, 2019, 2023 Nicholas Hayes
 //
 // This file is licensed under the MIT License.
 // See LICENSE.txt for complete licensing and attribution information.
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 //
 // This file is part of pdn-pspformat, a FileType plugin for Paint.NET
 //
-// Copyright (c) 2011-2015, 2019 Nicholas Hayes
+// Copyright (c) 2011-2015, 2019, 2023 Nicholas Hayes
 //
 // This file is licensed under the MIT License.
 // See LICENSE.txt for complete licensing and attribution information.
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("null54")]
 [assembly: AssemblyProduct("PaintShopProFiletype")]
-[assembly: AssemblyCopyright("Copyright © 2019 Nicholas Hayes (aka null54)")]
+[assembly: AssemblyCopyright("Copyright © 2023 Nicholas Hayes (aka null54)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
